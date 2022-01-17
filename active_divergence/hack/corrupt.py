9c3f474b052866765bbddfef2a5824bfd97bce79
Axel Chemla-Romeu-Santos <axel.chemla-romeu-santos@ircam.fr>
2022-01-17 20:01:35 +0100

    added hack routine

---
+++ active_divergence/hack/corrupt.py
@@ -0,0 +1,20 @@
+import torch, torch.nn as nn, numpy as np
+
+
+def normal_(parameter, mean=0.0, std=0.1):
+    parameter.data = parameter.data + mean + std * torch.randn_like(parameter.data)
+
+def salt_and_pepper_(parameter, prob=0.2):
+    mask = torch.full_like(parameter.data, prob).to(parameter.device)
+    noise = torch.bernoulli(1-mask)
+    parameter.data = parameter.data * noise
+
+def scale_(parameter, weight=1.0):
+    parameter.data = parameter.data * weight
+
+def bias_(parameter, bias=0.0):
+    parameter.data = parameter.data + bias
+
+def permute_(parameter, axis=0):
+    perm = torch.randperm(parameter.shape[axis])
+    parameter.data = parameter.data.index_select(axis, perm)
